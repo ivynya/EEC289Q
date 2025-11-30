474f0aab0d368277cdbb9592fee44f3bd3d6ee1e
ivynya <ivy@ivy.direct>
2025-11-30 12:59:30 -0800

    change graph datastructure to better fit solve format

--- pkg/graph.go
+++ pkg/graph.go
@@ -2,20 +2,16 @@ package tsp_solver
 
 // graph is an undirected weighted graph struct
 type Graph struct {
-	Nodes map[int]struct{}
-	Edges map[int][]Edge
-}
-
-type Edge struct {
-	To     int
-	Weight float64
+	edges map[int]map[int]float64
+	nodes []int
 }
 
 func NewGraph(nCount int) *Graph {
-	return &Graph{
-		Nodes: make(map[int]struct{}, 0),
-		Edges: make(map[int][]Edge, nCount),
+	g := Graph{
+		edges: make(map[int]map[int]float64, nCount),
+		nodes: make([]int, 0, nCount),
 	}
+	return &g
 }
 
 func (g *Graph) AddEdge(from, to int, weight float64) {
@@ -23,21 +19,25 @@ func (g *Graph) AddEdge(from, to int, weight float64) {
 	f := max(from, to)
 	t := min(from, to)
 
-	// minimal allocation to log node existence
-	g.Nodes[f] = struct{}{}
-	g.Nodes[t] = struct{}{}
-
-	// add the edge
-	g.Edges[f] = append(g.Edges[f], Edge{To: t, Weight: weight})
+	if g.edges[f] == nil {
+		g.edges[f] = make(map[int]float64)
+		g.nodes = append(g.nodes, f)
+	}
+	if g.edges[t] == nil {
+		g.edges[t] = make(map[int]float64)
+		g.nodes = append(g.nodes, t)
+	}
+	g.edges[f][t] = weight
+	g.edges[t][f] = weight
 }
 
 func (g *Graph) NodeCount() int {
-	return len(g.Nodes)
+	return len(g.nodes)
 }
 
 func (g *Graph) EdgeCount() int {
 	count := 0
-	for _, edges := range g.Edges {
+	for _, edges := range g.edges {
 		count += len(edges)
 	}
 	return count
